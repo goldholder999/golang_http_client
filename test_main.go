e8ddc8dc6cd7bcab6909048c23f3054557e4ef35
Lee Sungjin <goldholder999@gmail.com>
2021-05-01 00:19:34 +0900

    timeout option add

--- test_main.go
+++ test_main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net"
+	"time"
 )
 
 //const r_str = "GET %s HTTP/1.1\r\nHost: %s\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7\r\n\r\n"
 
 const r_str = "GET %s HTTP/1.1\r\n\r\n"
 
-func request_direct(url, addr string, port int) ([]byte, bool) {
+func request_direct(url, addr string, port int, timeout int) ([]byte, bool) {
 	ap := fmt.Sprintf("%s:%d", addr, port)
 	//request_bytes := []byte(fmt.Sprintf(r_str, url, ap))
 	request_bytes := []byte(fmt.Sprintf(r_str, url))
@@ -24,8 +26,18 @@ func request_direct(url, addr string, port int) ([]byte, bool) {
 	defer conn.Close()
 	conn.Write(request_bytes)
 	buf := make([]byte, 0, 4096) // big buffer
-	tmp := make([]byte, 256)     // using small tmo buffer for demonstrating
+	tmp := make([]byte, 4096)    // using small tmo buffer for demonstrating
+	duration := time.Duration(timeout) * time.Second
+
 	for {
+		if timeout > 0 {
+			err := conn.SetReadDeadline(time.Now().Add(duration))
+			if err != nil {
+				log.Println("SetReadDeadline failed:", err)
+				// do something else, for example create new conn
+				return nil, false
+			}
+		}
 		n, err := conn.Read(tmp)
 		if err != nil {
 			if err != io.EOF {
@@ -44,7 +56,7 @@ func main() {
 	url := "/download/"
 	addr := "tortoisegit.org"
 	port := 80
-	data, flag := request_direct(url, addr, port)
+	data, flag := request_direct(url, addr, port, 3)
 	if flag {
 		fmt.Println(string(data))
 	}
